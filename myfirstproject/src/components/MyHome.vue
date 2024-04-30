0cf5518767afcd14ec4033fff5a93ccf1cc270dd
Pedro Schumacher <pedrolschumacher@gmail.com>
2024-04-30 00:26:48 -0300

    FirstCommit

---
+++ myfirstproject/src/components/MyHome.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h3>meu Home</h3>
+        bem-vind{{ letraPorSexo }} {{ nome }}<br>
+        sexo {{ sexo }}
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'MyHome',
+    props: {
+        nome: {type: String, required: true},
+        sexo: {type: String, default: 'M'}
+    },
+    computed:{
+        letraPorSexo() {
+            if (this.sexo === 'F')
+                return 'a'
+            else
+                return 'o'
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ myfirstproject/src/components/PostTile.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="divPost">
+    <h2>{{ post.title }}</h2> <br>
+    <p>{{ post.body }}</p> <br>
+    por: {{ post.userId }}
+</div>
+</template>
+
+
+<script>
+export default {
+    name: 'PostTile',
+    props: {
+        post: {type: Object, required: true}
+    }
+}
+
+</script>
+
+<style>
+.divPost {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    margin: 1rem 0;
+}
+</style>
+
